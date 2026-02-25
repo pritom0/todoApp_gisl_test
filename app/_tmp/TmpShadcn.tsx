3edf902d9e9633ab67617518636ee0cb1f80e0e3
pritom0 <kurisubhajeet@gmail.com>
2026-02-25 14:43:48 +0600

    integrating shadcn

--- app/_tmp/TmpShadcn.tsx
+++ app/_tmp/TmpShadcn.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { ArrowUpIcon, CircleFadingArrowUpIcon } from "lucide-react";
 import { IconGitBranch, IconGitFork } from "@tabler/icons-react";
@@ -71,12 +73,43 @@ export default function TmpShadcn() {
         </Button>
       </div>
       <ButtonDemo />
+      <AlertDemo />
     </div>
   );
 }
 
 // property) variant?: "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | null | undefined
 //
+
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { CheckCircle2Icon, InfoIcon } from "lucide-react"
+
+export function AlertDemo() {
+  return (
+    <div className="grid w-full max-w-md items-start gap-4">
+      <Alert>
+        <CheckCircle2Icon />
+        <AlertTitle>Payment successful</AlertTitle>
+        <AlertDescription>
+          Your payment of $29.99 has been processed. A receipt has been sent to
+          your email address.
+        </AlertDescription>
+      </Alert>
+      <Alert>
+        <InfoIcon />
+        <AlertTitle>New feature available</AlertTitle>
+        <AlertDescription>
+          We&apos;ve added dark mode support. You can enable it in your account
+          settings.
+        </AlertDescription>
+      </Alert>
+      <SonnerDemo />
+      <Notification2 />
+      <Notification3 />
+    </div>
+  )
+}
 
 export function ButtonDemo() {
   return (
@@ -87,4 +120,67 @@ export function ButtonDemo() {
       </Button>
     </div>
   );
+}
+
+
+// import { Button } from "@/components/ui/button"
+import { toast } from "sonner"
+
+export function SonnerDemo() {
+  console.log("sonner test")
+  return (
+    <Button
+      variant="outline"
+      onClick={() =>
+        toast("Event has been created", {
+          description: "Sunday, December 03, 2023 at 9:00 AM",
+          action: {
+            label: "Undo",
+            onClick: () => console.log("Undo"),
+          },
+        })
+      }
+    >
+      Show Toast
+    </Button>
+  )
+}
+
+export function Notification2(){
+  return (
+    <>
+      <Button
+        onClick={()=> {
+          toast("toast at the top", {
+            description: "showing a pop up message",
+            position: "top-center",
+
+          },
+        )
+        }}
+      > pop up </Button>
+
+    </>
+  )
+}
+
+
+export function Notification3(){
+  return (
+    <>
+      <Button
+        onClick={()=> {
+          toast.promise<{name: string}> ( () => new Promise((resolve => setTimeout(()=>resolve({name: "a new event"}),1000))),
+          {
+              loading: "Loading...",
+              success: (data) => `${data.name} has been created`,
+              error: "Error",
+
+          } 
+        )
+        }}
+      > pop up </Button>
+
+    </>
+  )
 }
